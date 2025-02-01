931f6d1924dd4392bade4b68f0c8fae9b43d12c7
Bytegab <gabrielfreiresimon@gmail.com>
2025-02-01 19:31:57 +0100

    remove loop_hook

--- game.c
+++ game.c
@@ -1,47 +1,17 @@
-#include "minilibx-linux/mlx.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 17:43:03 by gafreire          #+#    #+#             */
+/*   Updated: 2025/02/01 19:19:39 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int close_window(int keycode, void *param)
-{
-    if (keycode == 65307)
-    {
-        printf("Close Window\n");
-        mlx_destroy_window(param, param);
-        exit(0);
-    }
-    return (0);
-}
+#include "game.h"
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-int key_handler(int keycode, t_vars *vars)
-{
-    if (keycode == 2)
-    {
-        printf("Key W\n");
-        // vars->player.pos.x += 1;
-    }
-    else if (keycode == 0)
-    {
-        printf("Key S\n");
-        // vars->player.pos.x -= 1;
-    }
-    else if (keycode == 13)
-    {
-        printf("Key A\n");
-        // vars->player.pos.y -= 1;
-    }
-    else if (keycode == 1)
-    {
-        printf("Key D\n");
-        // vars->player.pos.y += 1;
-    }
-    return (0);
-}
 
 int main(void)
 {
@@ -58,8 +28,8 @@ int main(void)
 	int		img_height2;
 
     // key handler
-    t_vars	vars;
-
+    // read_map
+    read_map();
     mlx = mlx_init();
     // open window
     mlx_win = mlx_new_window(mlx, 928, 793, "Paparoni esto es easy");
@@ -73,10 +43,9 @@ int main(void)
     img2 = mlx_xpm_file_to_image(mlx, relative_path2, &img_width2, &img_height2);
     mlx_put_image_to_window(mlx, mlx_win, img2, 1, 1);
     printf("Created walls\n");
-    // key handler
-    mlx_loop_hook(mlx_win,key_handler,&vars);
     // close window
-    mlx_key_hook(mlx_win, close_window, mlx_win); 
+    mlx_key_hook(mlx_win, close_window, mlx_win);
+    
     // finish loop
     mlx_loop(mlx);
     //  cc game.c libmlx_Linux.a -lX11 -lXext -o game 
