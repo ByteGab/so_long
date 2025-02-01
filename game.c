6682c3ef7e68abdb220c87690127f6d59544f064
Bytegab <gabrielfreiresimon@gmail.com>
2025-02-01 22:02:02 +0100

    add loop_wall

--- game.c
+++ game.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 17:43:03 by gafreire          #+#    #+#             */
-/*   Updated: 2025/02/01 20:13:27 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/02/01 21:42:22 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,33 @@ int main(void)
 	int		img_height;
     int		img_width2;
 	int		img_height2;
+    int     i;
+    int     z;
 
-    // key handler
+    i = 60;
+
+    
     // read_map
     read_map();
+    // key handler
+    
     mlx = mlx_init();
     // open window
     mlx_win = mlx_new_window(mlx, 928, 793, "Paparoni esto es easy");
     printf("Created Window\n");
     // show images
     // background
-    background_img = mlx_xpm_file_to_image(mlx, relative_path_background, &img_width, &img_height);
+    background_img = mlx_xpm_file_to_image(mlx, relative_path_background, &img_width, &img_height);  
     mlx_put_image_to_window(mlx, mlx_win, background_img, 0, 0);
     printf("Created Background\n");
     // walls
     img2 = mlx_xpm_file_to_image(mlx, relative_path2, &img_width2, &img_height2);
     mlx_put_image_to_window(mlx, mlx_win, img2, 1, 1);
+    while (i > 0)
+    {
+        mlx_put_image_to_window(mlx, mlx_win, img2, (i * 16),1);
+        i--;
+    }
     printf("Created walls\n");
     // close window
     mlx_key_hook(mlx_win, close_window, mlx_win);
