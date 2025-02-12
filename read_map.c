2a1c2d0f8d42b2e2993ab06a771eb562e76b8325
Bytegab <gabrielfreiresimon@gmail.com>
2025-02-12 16:07:00 +0100

    change read_map

--- read_map.c
+++ read_map.c
@@ -6,27 +6,28 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 18:41:44 by gafreire          #+#    #+#             */
-/*   Updated: 2025/02/12 13:23:00 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:04:02 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+// #include <string.h> // borrar
 
 void	read_map(char *map, t_vars *vars)
 {
 	int fd;
 	char *line;
 	int columns;
-	char *file;
+	char **file_map;
 
 	fd = open(map, O_RDONLY);
 	line = get_next_line(fd);
 	columns = 0;
+
+	// file_map = (char **)malloc(strlen(line) - 1);
 	while (line != NULL)
 	{
-		file = (char *)malloc(strlen(line) - 1);
-		file = line;
-		printf("%s",file);
+		// file_map[columns] = strdup(line);
 		printf("%s", line);
 		free(line);
 		printf("\n");
@@ -34,7 +35,20 @@ void	read_map(char *map, t_vars *vars)
 		columns++;
 		line = get_next_line(fd);
 	}
+	// int i;
+
+	// i = 0;
+	// while (columns > 0)
+	// {
+	// 	while (file_map[columns][i] != '\0')
+	// 	{
+	// 		write(1,&file_map[columns][i],1);
+	// 		i++;
+	// 	}
+	// 	columns--;
+	// }
+	
 	printf("\n");
-	printf("%d\n",columns);
+	// printf("%d\n",columns);
 	close(fd);
 }
