cd474b2949ae4b169a5f17ea4e9638d848a432f0
Bytegab <gabrielfreiresimon@gmail.com>
2025-02-12 10:49:27 +0100

    change draw and read map

--- draw_map.c
+++ draw_map.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read_map.c                                         :+:      :+:    :+:   */
+/*   draw_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/01 18:41:44 by gafreire          #+#    #+#             */
-/*   Updated: 2025/02/11 12:41:48 by gafreire         ###   ########.fr       */
+/*   Created: 2025/02/11 12:31:42 by gafreire          #+#    #+#             */
+/*   Updated: 2025/02/11 12:31:49 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	read_map(char *map, t_vars *vars)
+void	draw_map(char *map, t_vars *vars)
 {
-	int fd;
-	char *line;
-	char	**readed_map;
-	int columns;
-	int	i;
 
-	fd = open(map, O_RDONLY);
-	line = get_next_line(fd);
-	i = 0;
-	while (line != NULL)
-	{
-		// readed_map[i];
-		printf("%s", line);
-		free(line);
-		i++;
-		
-		line = get_next_line(fd);
-	}
-	close(fd);
 }
