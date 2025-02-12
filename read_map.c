641809d6589ca0f3f05423a9a169c40b15f50c6e
Bytegab <gabrielfreiresimon@gmail.com>
2025-02-12 14:02:34 +0100

    change read_mao

--- read_map.c
+++ read_map.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 18:41:44 by gafreire          #+#    #+#             */
-/*   Updated: 2025/02/11 12:41:48 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:23:00 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,25 @@ void	read_map(char *map, t_vars *vars)
 {
 	int fd;
 	char *line;
-	char	**readed_map;
 	int columns;
-	int	i;
+	char *file;
 
 	fd = open(map, O_RDONLY);
 	line = get_next_line(fd);
-	i = 0;
+	columns = 0;
 	while (line != NULL)
 	{
-		// readed_map[i];
+		file = (char *)malloc(strlen(line) - 1);
+		file = line;
+		printf("%s",file);
 		printf("%s", line);
 		free(line);
-		i++;
-		
+		printf("\n");
+		// free(file);
+		columns++;
 		line = get_next_line(fd);
 	}
+	printf("\n");
+	printf("%d\n",columns);
 	close(fd);
 }
