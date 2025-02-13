a2b22621c7f5f2802289c353f05f9710f7c5d8ca
Bytegab <gabrielfreiresimon@gmail.com>
2025-02-13 16:31:38 +0100

    add moves to character

--- loop_hook.c
+++ loop_hook.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 17:43:14 by gafreire          #+#    #+#             */
-/*   Updated: 2025/02/13 14:08:26 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:30:58 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,42 +20,35 @@ int	loop_hook(int keycode, t_vars *vars)
         printf("Close Window\n");
         exit(0);
         mlx_destroy_window(vars, vars);
-        
     }
     else if (keycode == 119)
     {
         printf("Key W\n");
-        mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, vars->pos_x, 200);
-        vars->pos_x += 64;
-        mlx_put_image_to_window(vars->mlx, vars->win, vars->player, vars->pos_x, 200);
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, vars->pos_x, vars->pos_y);
+        vars->pos_y -= 64;
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->player, vars->pos_x, vars->pos_y);
     }
     else if (keycode == 115)
     {
         printf("Key S\n");
-        mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, vars->pos_x, 200);
-        vars->pos_x -= 64;
-        mlx_put_image_to_window(vars->mlx, vars->win, vars->player, vars->pos_x, 200);
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, vars->pos_x, vars->pos_y);
+        vars->pos_y += 64;
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->player, vars->pos_x, vars->pos_y);
     }
     else if (keycode == 97)
     {
         printf("key A\n");
-        // vars->player.pos.y -= 1;
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, vars->pos_x, vars->pos_y);
+        vars->pos_x -= 64;
+        // mlx_put_image_to_window(vars->mlx, vars->win, vars->player_move, vars->pos_x, vars->pos_y);
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->player, vars->pos_x, vars->pos_y);
     }
     else if (keycode == 100)
     {
         printf("Key D\n");
-        // vars->player.pos.y += 1;
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, vars->pos_x, vars->pos_y);
+        vars->pos_x += 64;
+        mlx_put_image_to_window(vars->mlx, vars->win, vars->player_move, vars->pos_x, vars->pos_y);
     }
     return (0);
 }
-
-// int	main(void)
-// {
-// 	t_vars	vars;
-
-// 	vars.mlx = mlx_init();
-// 	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-// 	mlx_key_hook(vars.win, loop_hook, &vars);
-
-// 	mlx_loop(vars.mlx);
-// }
