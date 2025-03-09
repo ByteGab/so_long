eccd5c95801fab00212b43bd8fcdc06d48a62214
Bytegab <gabrielfreiresimon@gmail.com>
2025-03-09 18:36:24 +0100

    ok norminette

--- draw_map.c
+++ draw_map.c
@@ -6,102 +6,74 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 12:31:42 by gafreire          #+#    #+#             */
-/*   Updated: 2025/03/09 05:04:41 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/03/09 18:35:41 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	check_items(t_vars *vars)
+{
+	if (vars->count_coins == 0)
+		error_item(vars);
+	if (vars->count_exit == 0 || vars->count_exit > 1)
+		error_item(vars);
+	if (vars->count_player == 0 || vars->count_player > 1)
+		error_item(vars);
+}
+
+void	put_img_and_count(char item, t_vars *vars, int i, int j)
+{
+	if (item == 'P')
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->player, j * 64, i
+			* 64);
+	else if (item == '0')
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->background, j * 64,
+			i * 64);
+	else if (item == '1')
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, j * 64, i
+			* 64);
+	else if (item == 'C')
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->coins, j * 64, i
+			* 64);
+	else if (item == 'E')
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->exit, j * 64, i
+			* 64);
+}
+
+void	put_img(char item, t_vars *vars, int i, int j)
+{
+	if (item == 'P')
+	{
+		vars->pos_x = j * 64;
+		vars->pos_y = i * 64;
+		vars->count_player++;
+	}
+	else if (item == 'C')
+		vars->count_coins++;
+	else if (item == 'E')
+		vars->count_exit++;
+	else if (item != '0' && item != '1' && item != 'E' && item != 'C'
+		&& item != 'P')
+		error_item(vars);
+	put_img_and_count(item, vars, i, j);
+}
+
 void	place_textures(t_vars *vars)
 {
-	int	img_width;
-	int	img_height;
 	int	i;
 	int	j;
 
 	i = 0;
-	vars->background = mlx_xpm_file_to_image(vars->mlx, "assets/Grass.xpm",
-			&img_width, &img_height);
-	vars->walls = mlx_xpm_file_to_image(vars->mlx, "assets/water.xpm",
-			&img_width, &img_height);
-	vars->player = mlx_xpm_file_to_image(vars->mlx, "assets/Dino.xpm",
-			&img_width, &img_height);
-	vars->player_move = mlx_xpm_file_to_image(vars->mlx, "assets/Dino_move.xpm",
-			&img_width, &img_height);
-	vars->exit = mlx_xpm_file_to_image(vars->mlx, "assets/House.xpm",
-			&img_width, &img_height);
-	vars->coins = mlx_xpm_file_to_image(vars->mlx, "assets/Egg.xpm", &img_width,
-			&img_height);
-	vars->count_coins = 0;
-	vars->count_moves = 0;
-	vars->count_exit = 0;
-	vars->count_player = 0;
 	while (i < vars->rows)
 	{
 		j = 0;
 		while (j < vars->columns)
 		{
-			if (vars->map[i][j] != 'P' && vars->map[i][j] != '0'
-				&& vars->map[i][j] != '1' && vars->map[i][j] != 'C'
-				&& vars->map[i][j] != 'E')
-			{
-				printf("El mapa no es valido");
-				mlx_destroy_window(vars->mlx, vars->win);
-				clean_up(vars);
-				exit(0);
-			}
-			else if (vars->map[i][j] == 'P')
-			{
-				mlx_put_image_to_window(vars->mlx, vars->win, vars->player, j
-					* 64, i * 64);
-				vars->pos_x = j * 64;
-				vars->pos_y = i * 64;
-				vars->count_player++;
-			}
-			else if (vars->map[i][j] == '0')
-				mlx_put_image_to_window(vars->mlx, vars->win, vars->background,
-					j * 64, i * 64);
-			else if (vars->map[i][j] == '1')
-				mlx_put_image_to_window(vars->mlx, vars->win, vars->walls, j
-					* 64, i * 64);
-			else if (vars->map[i][j] == 'C')
-			{
-				mlx_put_image_to_window(vars->mlx, vars->win, vars->coins, j
-					* 64, i * 64);
-				vars->count_coins++;
-			}
-			else if (vars->map[i][j] == 'E')
-			{
-				mlx_put_image_to_window(vars->mlx, vars->win, vars->exit, j
-					* 64, i * 64);
-				vars->count_exit++;
-			}
+			put_img(vars->map[i][j], vars, i, j);
 			j++;
 		}
 		i++;
 	}
-	// not coins
-	if (vars->count_coins == 0)
-	{
-		printf("El mapa no es valido");
-		mlx_destroy_window(vars->mlx, vars->win);
-		clean_up(vars);
-		exit(0);
-	}
-	// error exit
-	if (vars->count_exit == 0 || vars->count_exit > 1)
-	{
-		printf("El mapa no es valido");
-		mlx_destroy_window(vars->mlx, vars->win);
-		clean_up(vars);
-		exit(0);
-	}
-	// error player
-	if (vars->count_player == 0 || vars->count_player > 1)
-	{
-		printf("El mapa no es valido");
-		mlx_destroy_window(vars->mlx, vars->win);
-		clean_up(vars);
-		exit(0);
-	}
+	check_items(vars);
 }
